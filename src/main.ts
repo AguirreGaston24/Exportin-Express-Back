bde94a3057e3f885c70519588785f7fb343908e8
AguirreGaston24 <aguirregastona@hotmail.com>
2024-12-22 14:09:32 -0300

    22/12

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   // Configuración de CORS para permitir credenciales
   app.enableCors({
-    origin: process.env.DEPLOY,  // Permite solo tu frontend
+    origin: 'https://exportingexperts.pro',  // Permite solo tu frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'],  // Cabeceras permitidas
     credentials: true,  // Permite el uso de credenciales (cookies, autorización)
